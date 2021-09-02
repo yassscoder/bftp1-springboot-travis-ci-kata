caa5fcedc9debd08db2076eb57ae21f4a5fa2282
Javi <javi@jsrois.net>
2021-09-02 19:37:43 +0200

    Initial commit

---
+++ src/main/java/org/factoriaf5/app/Bftp1SpringbootTravisCiKataApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bftp1SpringbootTravisCiKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bftp1SpringbootTravisCiKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/factoriaf5/app/Bftp1SpringbootTravisCiKataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bftp1SpringbootTravisCiKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
